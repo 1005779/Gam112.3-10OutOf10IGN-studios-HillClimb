7f1be1aef045a58b767425a6632e1c27db74cbf2
Tyler Quick <tquick@sae.au>
2016-07-21 17:42:51 +1000

    Final push for class

---
+++ Assets/Scripts/Cart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cart : MonoBehaviour {
+
+    Rigidbody RB;
+
+    public float force = 1000.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        RB = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Movement()
+    {
+       if(Input.GetKeyDown("d"))
+        {
+            
+        }
+    }
+}
